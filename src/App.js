ef40f0ddcbe97f9d44418a250315041e88a1b7d3
DimBoiko <boykodima123@ukr.net>
2022-09-03 13:52:23 +0300

    deploy-test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import MainPage from "./pages/MainPage";
-import {BrowserRouter,Route,Routes} from 'react-router-dom' 
-import AdminPage from "./pages/AdminPage";
-import LoginPage from "./pages/LoginPage";
 
 function App() {
 	
   return (
 	  <div className="App">
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<MainPage/>}/>
-				<Route path="/ad" element={<AdminPage/>}/>
-				<Route path="/login" element={<LoginPage/>}/>
-			</Routes>
-		</BrowserRouter>
+		<MainPage/>
     </div>
   );
 }
