865d2b0c084f0a5fe8b0ec49599784fa3364d1f0
Dima <boikod887@gmail.com>
2024-01-02 19:30:16 +0200

    update

--- src/components/support/Support.jsx
+++ src/components/support/Support.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './support.css'
 import viber from '../../img/viber.png'
-import telegram from '../../img/telegram.png'
 import whatsapp from '../../img/whatsapp.png'
 import gmail from '../../img/gmail-icon.svg'
 import { Transition } from 'react-transition-group';
